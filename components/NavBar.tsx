513b7286c9787bb7997972aea21f8b674a9180ad
takeru-a <pypy20191022@gmail.com>
2023-04-19 06:32:49 +0900

    add:homePage

---
+++ components/NavBar.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Link from 'next/link'
+import Logo from './Logo'
+import { useRouter } from 'next/router'
+import InstagramIcon, { GithubIcon } from './Icon'
+import { motion } from 'framer-motion';
+
+const CustomLink = ({href, title, className=''}: {href: string, title: string, className?: string}) =>{
+  const router = useRouter();
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+
+      <span className={`h-[1px] inline-block w-0 bg-black
+      absolute left-0 -bottom-0.5
+      group-hover:w-full transition-[width] ease duration-300
+      ${router.asPath === href ? 'w-full' : 'w-0'}
+      `}>&nbsp;</span>
+    </Link>
+  )
+
+}
+
+const NavBar = () => {
+  return (
+    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'
+    >
+      <nav>
+        <CustomLink href={"/"} title={"Home"} className='mr-4'/>
+        <CustomLink href={"/about"} title={"About"} className='mx-4'/>
+        <CustomLink href={"/projects"} title={"Projects"} className='mx-4'/>
+        <CustomLink href={"/articles"} title={"Articles"} className='ml-4'/>
+      </nav>
+      
+      <nav className='flex items-center justify-center flex-wrap'>
+        <motion.a href="https://www.instagram.com/takekod13/" target={"_blank"} 
+        className=' w-6 mr-6 wh'
+        whileHover={{y:-2}}
+        whileTap={{scale:0.9}}
+        >
+        <InstagramIcon />
+        </motion.a>
+        <motion.a href="https://github.com/takeru-a/" target={"_blank"}
+         className=' w-6 mr-3 wh'
+         whileHover={{y:-2}}
+         whileTap={{scale:0.9}}
+        >
+        <GithubIcon/>
+        </motion.a>
+      </nav>
+
+      <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+      <Logo/>      
+      </div>
+    </header>
+  )
+}
+
+export default NavBar
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,8 @@
+import Layout from '@/components/Layout'
 import Head from 'next/head'
+import Image from 'next/image'
+import profileImg from '../../mypage/public/shiba.png'
+import AnimatedText from '@/components/AnimatedText'
 
 
 export default function Home() {
@@ -8,8 +12,20 @@ export default function Home() {
         <title>profile</title>
        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       </Head>
-      <main className=''>
-        Home
+      <main className='flex items-center text-dark w-full min-h-screen'>
+        <Layout className='pt-0'>
+          <div className=' flex items-center justify-between w-full'>
+              <div className='w-1/2'>
+                <Image src={profileImg} alt='tk'className='w-full h-auto' />
+              </div>
+          <div className='w-1/2 flex flex-col items-center self-center'>
+            <AnimatedText text='Bringing Vision To Life With Code And Ideas.' className='!text-6xl !text-left'/>
+            <p className=' my-4 text-base font-medium'>
+            I&apos;m Takeru, a software engineer who loves tackling complex problems and creating innovative solutions through code.
+            </p>
+          </div>
+        </div>
+        </Layout>
       </main>
       </>
   )
